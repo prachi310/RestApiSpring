33d458d41619d2c41385e96ebd58d991479b0d1e
Prachi <prachipande24@gmail.com>
2024-04-01 08:33:08 +0530

    added more api

--- RestApiSpring/src/main/java/com/example/RestApiSpring/TodoController.java
+++ RestApiSpring/src/main/java/com/example/RestApiSpring/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.RestApiSpring;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +17,31 @@ public class TodoController {
 
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
-        this.toDoList=new ArrayList<>();
-        toDoList.add(new ToDo(1,true,"ToDo1",1));
-        toDoList.add(new ToDo(2,false,"ToDo2",2));
+        this.toDoList = new ArrayList<>();
+        toDoList.add(new ToDo(1, true, "ToDo1", 1));
+        toDoList.add(new ToDo(2, false, "ToDo2", 2));
     }
 
     @GetMapping
-    public ResponseEntity<List<ToDo>> getTodos()
-    {
-        System.out.println("method invoked : " +this.todoService.doSomething());
+    public ResponseEntity<List<ToDo>> getTodos() {
+        System.out.println("method invoked : " + this.todoService.doSomething());
         return ResponseEntity.ok(toDoList);
     }
 
+    @PostMapping("/createTodo")
+    public ResponseEntity<List<ToDo>> createToDo(@RequestBody ToDo newToDo) {
+        toDoList.add(newToDo);
+        return ResponseEntity.ok(toDoList);
+    }
+
+    @GetMapping("/{Id}")
+    public ResponseEntity<?> getById(@PathVariable Integer id) {
+
+        for (ToDo todo : toDoList) {
+            if (todo.getId() == id)
+                return ResponseEntity.ok(todo);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ToDo not found");
+    }
 
 }
